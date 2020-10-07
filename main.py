8d2133248263de1d5bddd5ea1e756a9663f180ab
Louis Dupont <contact@lyro.fr>
2020-10-07 16:51:31 +0200

    Added better visualisation

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ import browserhistory as bh
 import requests
 
 
-def check_url(url):
-    print("Testing " + url + "...")
+
+def check_url(current, total, url):
     r = requests.post("https://urlhaus-api.abuse.ch/v1/" + url)
+    print(str(current) + '/' + str(total))
+
     if r.status_code == 404:
-        return None
+        return current + 1, None
     else:
-        print(r.json())
-        return r.json()
+        return current + 1, r.json()
 
 
 if __name__ == '__main__':
     history = bh.get_browserhistory()
+    total = 0
+    for browser in history:
+        total += len(history[browser])
+    current = 1
+    foundDanger = 0
+
+    if total <= 0:
+        print("No entry found.")
+
     for browser in history:
         for entry in history[browser]:
-            check_url(entry[0])
-    else:
-        print("No browsers available")
+            current, json = check_url(current, total, entry[0])
+            if json:
+                foundDanger += 1
+                print(json)
 
+    print("Results: " + str(foundDanger) + " dangerous URLs found.")
