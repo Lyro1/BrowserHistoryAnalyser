6c648ad4aa8ee07cadcaf892d672c37950474fad
Louis Dupont <contact@lyro.fr>
2020-10-07 16:01:54 +0200

    First push

---
+++ main.py
@@ -0,0 +1,12 @@
+import browserhistory as bh
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    dict_obj = bh.get_browserhistory()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
