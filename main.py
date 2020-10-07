0813411ab9c9bfbb6324486b87140a37856d49fa
Louis Dupont <contact@lyro.fr>
2020-10-07 16:22:23 +0200

    Testing all URLs

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import requests
 
 def check_url(url):
     r = requests.post("https://urlhaus-api.abuse.ch/v1/" + url)
-    print(r.json)
+    if r.status_code != 404:
+        return None
+    else:
+        print(r.json)
+        return r.json
 
 
 if __name__ == '__main__':
     history = bh.get_browserhistory()
     for browser in history:
-        print("Testing " + history[browser][0][0] + "...")
-        check_url(history[browser][0][0])
+        for entry in history[browser]:
+            check_url(entry[0])
     else:
         print("No browsers available")
 
