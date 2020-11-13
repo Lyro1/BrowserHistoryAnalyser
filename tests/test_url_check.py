bd75146b7f11404fc99fc9f8a1d84b0d4b55f589
Louis Dupont <contact@lyro.fr>
2020-11-13 01:56:31 +0100

    Removed useless print

--- tests/test_url_check.py
+++ tests/test_url_check.py
@@ -7,7 +7,6 @@ from src.Entities.HistoryEntry import HistoryEntry
 async def test_url_haus_flagged():
     entry = HistoryEntry("chrome", "http://182.59.75.73:42447/bin.sh")
     await check_urlhaus(entry, "https://urlhaus-api.abuse.ch/v1/url/")
-    print("yet")
     assert entry.flagged.url_haus is True
 
 
@@ -15,5 +14,4 @@ async def test_url_haus_flagged():
 async def test_url_haus_clean():
     entry = HistoryEntry("chrome", "https://google.com")
     await check_urlhaus(entry, "https://urlhaus-api.abuse.ch/v1/url/")
-    print("yet")
     assert entry.flagged.url_haus is False
