8a89e2be4097842b949f014f3018f2a93643b31b
taillant <pmtaillantou@gmail.com>
2020-11-29 11:49:48 +0100

    add param in event to close windows and modif file

--- Tkinter.py
+++ Tkinter.py
@@ -5,12 +5,7 @@ import main
 from src.Entities.ConfigEntry import ConfigEntry
 from src.Entities.HistoryEntryFlags import HistoryEntryFlags
 
-
-def start():
-    root.event_generate("<<start>>")
-    button_start.config(state="disable")
-
-def start_event(evt):
+def start_event():
     main.main()
     #test flag positif
     '''flags = HistoryEntryFlags()
@@ -21,8 +16,13 @@ def start_event(evt):
     result = tkinter.Label()
     result.config(text=main.result())
     result.pack()
+    button_start.config(state="disable")
 
 
+def validParam_event(limit_entries, max_entries, windows):
+    main.config.modif_file(limit_entries=limit_entries,max_entries=max_entries)
+    print("toto")
+    windows.destroy()
 
 
 
@@ -32,16 +32,17 @@ def modifParam():
     button_limit_entries.pack()
     print(button_limit_entries.instate(['selected']))
 
-    '''J'en suis ici
-    Il faut faire un bouton pour valider les paramètres
-    il faut ensuite faire les boutons permettant de modifier les paramètre'''
+    #J'en suis ici
+    #Il faut faire un bouton pour valider les paramètres
+    ##il faut ensuite faire les boutons permettant de modifier les paramètre
 
-    button_valid_param = tkinter.Button(paramWindows,text="valider param", command=validParam)
-    button_change_param.pack()
-
+    button_valid_param = tkinter.Button(paramWindows, text="valider param", command=lambda: validParam_event(limit_entries=True, max_entries=10, windows=paramWindows))
+    button_valid_param.pack()
     result = tkinter.Label()
     result.config(text="param modifié")
     result.pack()
+
+
 
 
 root = tkinter.Tk()
@@ -50,14 +51,18 @@ title = tkinter.Label (text = "Welcome in Browser History Analyser")
 title.pack()
 
 
-button_start = tkinter.Button(text = "START", command=start)
+button_start = tkinter.Button(text = "START", command=start_event)
 button_start.pack()
+
+
 
 button_change_param = tkinter.Button(text="modifier param", command=modifParam)
 button_change_param.pack()
 
 
+button_close = tkinter.Button(text= "close", command=root.destroy)
+button_close.pack()
 
 
-root.bind("<<start>>", start_event)
+
 root.mainloop()
