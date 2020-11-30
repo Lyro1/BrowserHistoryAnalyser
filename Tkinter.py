9a3956f5cb1a38466eb4fc5a6057472076ded1ff
Louis Dupont <contact@lyro.fr>
2020-11-30 10:49:04 +0100

    Modified the window title

--- Tkinter.py
+++ Tkinter.py
@@ -5,9 +5,10 @@ import main
 from src.Entities.ConfigEntry import ConfigEntry
 from src.Entities.HistoryEntryFlags import HistoryEntryFlags
 
+
 def start_event():
     main.main()
-    #test flag positif
+    # test flag positif
     '''flags = HistoryEntryFlags()
     flags.url_haus = True
     entry = main.HistoryEntry("http://182.59.75.73:42447/bin.sh")
@@ -17,10 +18,11 @@ def start_event():
     button_start.config(state="disable")
 
 
-def validParam_event(windows,limit_entries, max_entries, max_threads, url_haus_conf, virus_total_conf):
+def validParam_event(windows, limit_entries, max_entries, max_threads, url_haus_conf, virus_total_conf):
     main.config.modif_file(limit_entries.get(), max_entries, max_threads, url_haus_conf, virus_total_conf, "src")
     windows.destroy()
     result.config(text="param modifié")
+
 
 def annulParam_event(windows):
     windows.destroy()
@@ -33,37 +35,33 @@ def modifParam():
     case = tkinter.Checkbutton(paramWindows, text="Limiter le nombre d'entrées", variable=var_case)
     case.pack()
 
-
-
-    max_entries=10
-    max_threads=50
-    url_haus_conf=None
-    virus_total_conf=None
-    button_valid_param = tkinter.Button(paramWindows, text="valider param", command=lambda: validParam_event(paramWindows,var_case, max_entries, max_threads, url_haus_conf, virus_total_conf))
+    max_entries = 10
+    max_threads = 50
+    url_haus_conf = None
+    virus_total_conf = None
+    button_valid_param = tkinter.Button(paramWindows, text="valider param",
+                                        command=lambda: validParam_event(paramWindows, var_case, max_entries,
+                                                                         max_threads, url_haus_conf, virus_total_conf))
     button_valid_param.pack()
-    button_reset_param = tkinter.Button(paramWindows, text="Annuler modification", command=lambda: annulParam_event(paramWindows))
+    button_reset_param = tkinter.Button(paramWindows, text="Annuler modification",
+                                        command=lambda: annulParam_event(paramWindows))
     button_reset_param.pack()
     paramWindows.mainloop()
 
 
-
-
 root = tkinter.Tk()
+root.title('Browser History Analyser')
 
-title = tkinter.Label (text = "Welcome in Browser History Analyser")
+title = tkinter.Label(text="Welcome in Browser History Analyser")
 title.pack()
 
-
-button_start = tkinter.Button(text = "START", command=start_event)
+button_start = tkinter.Button(text="START", command=start_event)
 button_start.pack()
-
-
 
 button_change_param = tkinter.Button(text="modifier param", command=modifParam)
 button_change_param.pack()
 
-
-button_close = tkinter.Button(text= "close", command=root.destroy)
+button_close = tkinter.Button(text="close", command=root.destroy)
 button_close.pack()
 
 result = tkinter.Label()
