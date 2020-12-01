cecd6da0d190dd39e4c08a03af4234559ca0d02e
taillant <pmtaillantou@gmail.com>
2020-12-01 12:30:49 +0100

    Ajout d'un champs de modification du nombre d'entrée avec Int uniquement

--- Tkinter.py
+++ Tkinter.py
@@ -19,7 +19,7 @@ def start_event():
 
 
 def validParam_event(windows, limit_entries, max_entries, max_threads, url_haus_conf, virus_total_conf):
-    main.config.modif_file(limit_entries.get(), max_entries, max_threads, url_haus_conf, virus_total_conf, "src")
+    main.config.modif_file(limit_entries.get(), max_entries.get(), max_threads, url_haus_conf, virus_total_conf, "src")
     windows.destroy()
     result.config(text="param modifié")
 
@@ -28,19 +28,31 @@ def annulParam_event(windows):
     windows.destroy()
 
 
+
+def only_numeric_input(P):
+    if P.isdigit() or P == "":
+        return True
+    return False
+
+
+
 def modifParam():
     paramWindows = tkinter.Toplevel(root)
-    var_case = tkinter.BooleanVar(paramWindows)
-    var_case.set(True)
-    case = tkinter.Checkbutton(paramWindows, text="Limiter le nombre d'entrées", variable=var_case)
-    case.pack()
-
-    max_entries = 10
+    limit_entries = tkinter.BooleanVar(paramWindows)
+    limit_entries.set(True)
+    limit_entries_checkbox = tkinter.Checkbutton(paramWindows, text="Limiter le nombre d'entrées", variable=limit_entries)
+    limit_entries_checkbox.pack()
+    max_entries = tkinter.IntVar(paramWindows)
+    max_entries.set(100)
+    func_callback = paramWindows.register(only_numeric_input)
+    max_entries_zone = tkinter.Entry(paramWindows, text="Nombre d'entrées limites", textvariable=max_entries, validate='all', validatecommand=(func_callback, "%P"))
+    max_entries_zone.pack()
     max_threads = 50
+
     url_haus_conf = None
     virus_total_conf = None
     button_valid_param = tkinter.Button(paramWindows, text="valider param",
-                                        command=lambda: validParam_event(paramWindows, var_case, max_entries,
+                                        command=lambda: validParam_event(paramWindows, limit_entries, max_entries,
                                                                          max_threads, url_haus_conf, virus_total_conf))
     button_valid_param.pack()
     button_reset_param = tkinter.Button(paramWindows, text="Annuler modification",
